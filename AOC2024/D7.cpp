985b866081c70785afce2f03d69ba05dfe565469
Hoff22 <abithoff@gmail.com>
2024-12-07 16:01:17 -0300

    day7

---
+++ AOC2024/D7.cpp
@@ -0,0 +1,275 @@
+// #include <bits/stdc++.h>
+
+//mac-os
+#include<vector>
+#include<algorithm>
+#include<iostream>
+#include<queue>
+#include<string>
+#include<set>
+
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+#define INFll 0x3f3f3f3f3f3f3f3fll
+#define se second
+#define fi first
+
+#define LLMAX 9223372036854775807
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+using namespace std;
+
+#define N 10000
+
+struct Bigint {
+    string a;
+    int sign;
+
+    Bigint(){}
+    void operator = (string b) {
+        a= (b[0]=='-' ? b.substr(1) : b);
+        reverse(a.begin(), a.end());
+        (*this).Remove0(b[0]=='-' ? -1 : 1);
+    }
+    Bigint(string x) {(*this)=x;}
+    Bigint(ll x) {(*this)=to_string(x);}
+    void operator = (ll x){*this=to_string(x);}
+
+    char operator[](int i){return a[i];}
+    int size() {return a.size();}
+    Bigint inverseSign() {sign*=-1; return (*this);}
+
+    Bigint Remove0(int newSign) {
+        sign = newSign;
+        for(int i=a.size()-1; i>0 && a[i]=='0'; i--) a.pop_back();
+        if(a.size()==1 && a[0]=='0') sign=1;
+        return (*this);
+    }
+
+    bool operator == (Bigint x) {return sign==x.sign && a==x.a;}
+    bool operator == (string x) {return *this==Bigint(x);}
+    bool operator == (ll x)     {return *this==Bigint(x);}
+    bool operator != (Bigint x) {return !(*this==x);}
+    bool operator != (string x) {return !(*this==x);}
+    bool operator != (ll x)     {return !(*this==x);}
+
+    bool operator < (Bigint b) {
+        if (sign!=b.sign) return sign<b.sign;
+        if(a.size()!=b.size()) return a.size()*sign<b.size()*sign;
+        for(int i=a.size()-1; i>=0; i--)
+            if(a[i] != b[i]) return a[i]<b[i];
+        return false;
+    }
+    bool operator <  (string x) {return *this<Bigint(x);}
+    bool operator <  (ll x)     {return *this<Bigint(x);}
+    bool operator <= (Bigint b) {return *this==b || *this<b;}
+    bool operator <= (string b) {return *this==b || *this<b;}
+    bool operator <= (ll b)     {return *this==b || *this<b;}
+    bool operator >  (Bigint b) {return !(*this==b || *this<b);}
+    bool operator >  (string x) {return !(*this==x || *this<x);}
+    bool operator >  (ll b)     {return !(*this==b || *this<b);}
+    bool operator >= (Bigint b) {return *this==b || *this>b;}
+    bool operator >= (string b) {return *this==b || *this>b;}
+    bool operator >= (ll b)     {return *this==b || *this>b;}
+
+    Bigint operator + (Bigint b) {
+        if(sign != b.sign) return (*this)-b.inverseSign();
+        Bigint sum;
+        for(int i=0, carry=0; i<a.size() || i<b.size() || carry; i++){
+            if (i<a.size()) carry+=a[i]-'0';
+            if (i<b.size()) carry+=b[i]-'0';
+            sum.a += (carry % 10 + 48);
+            carry /= 10;
+        }
+        return sum.Remove0(sign);
+    }
+    Bigint operator +  (string x) {return *this+Bigint(x);}
+    Bigint operator +  (ll x)     {return *this+Bigint(x);}
+    Bigint operator ++ (int) {*this+=1; return *this-1;}
+    Bigint operator ++ ()    {*this+=1; return *this;}
+      void operator += (Bigint x) {*this = *this+x;}
+      void operator += (string x) {*this = *this+x;}
+      void operator += (ll x)     {*this = *this+x;}
+
+
+    Bigint operator - ( Bigint b ) {
+        if(sign != b.sign) return (*this)+b.inverseSign();
+        if(*this < b) return (b - *this).inverseSign();
+        Bigint sub;
+        for(int i=0,borrow=0; i<a.size(); i++) {
+            borrow = a[i]-borrow-(i<b.size() ? b.a[i] : '0');
+            sub.a += borrow>=0 ? borrow+'0' : borrow + 58;
+            borrow = borrow>=0 ? 0:1;
+        }
+        return sub.Remove0(sign);
+    }
+    Bigint operator - (string x) {return *this-Bigint(x);}
+    Bigint operator - (ll x)     {return *this-Bigint(x);}
+    Bigint operator -- (int) {*this-=1; return *this+1;}
+    Bigint operator -- ()    {*this-=1; return *this;}
+      void operator -= (Bigint x) {*this = *this-x;}
+      void operator -= (string x) {*this = *this-x;}
+      void operator -= (ll x)     {*this = *this-x;}
+
+    Bigint operator * (Bigint b) {
+        Bigint mult("0");
+        for(int i=0, k=a[i]; i<a.size(); i++, k=a[i]) {
+            while(k-- -'0') mult=mult+b;
+            b.a.insert(b.a.begin(),'0');
+        }
+        return mult.Remove0(sign * b.sign);
+    }
+    Bigint operator * (string x) {return *this*Bigint(x);}
+    Bigint operator * (ll x)     {return *this*Bigint(x);}
+      void operator *= (Bigint x) {*this = *this*x;}
+      void operator *= (string x) {*this = *this*x;}
+      void operator *= (ll x)     {*this = *this*x;}
+
+    Bigint operator / (Bigint b) {
+        if(b.size()==1 && b[0]=='0') b.a[0]/=(b[0]-'0');
+        Bigint c("0"), d;
+        for(int j=0; j<a.size(); j++) d.a += "0";
+        int dSign = sign*b.sign; b.sign=1;
+        for(int i=a.size()-1; i>=0; i--) {
+            c.a.insert(c.a.begin(),'0');
+            c=c+a.substr(i,1);
+            while(!(c<b)) c=c-b, d.a[i]++;
+        }
+        return d.Remove0(dSign);
+    }
+    Bigint operator / (string x) {return *this/Bigint(x);}
+    Bigint operator / (ll x)     {return *this/Bigint(x);}
+      void operator /= (Bigint x) {*this = *this/x;}
+      void operator /= (string x) {*this = *this/x;}
+      void operator /= (ll x)     {*this = *this/x;}
+
+    Bigint operator % (Bigint b) {
+        if( b.size()==1 && b[0]=='0') b.a[0]/=(b[0]-'0') ;
+        Bigint c("0");
+        int cSign = sign*b.sign; b.sign=1;
+        for( int i=a.size()-1; i>=0; i-- ) {
+            c.a.insert( c.a.begin(),'0');
+            c = c+a.substr(i,1);
+            while(!(c<b)) c=c-b;
+        }
+        return c.Remove0(cSign);
+    }
+    Bigint operator % (string x) {return *this%Bigint(x);}
+    Bigint operator % (ll x)     {return *this%Bigint(x);}
+      void operator %= (Bigint x) {*this = *this%x;}
+      void operator %= (string x) {*this = *this%x;}
+      void operator %= (ll x)     {*this = *this%x;}
+
+    void print() {
+        if(sign==-1) putchar('-');
+        for(int i=a.size()-1; i>=0; i--) putchar(a[i]);
+    }
+    friend istream& operator >>(istream &in,Bigint &x){
+        string s; in>>s; x=s; return in;
+    }
+    friend ostream& operator <<(ostream &out,Bigint &x){
+        if(x.sign==-1) out<<('-');
+        for(int i=x.size()-1; i>=0; i--)
+            out<<(x[i]);
+        return out;
+    }
+
+    friend Bigint pow(Bigint base, Bigint pw){
+        Bigint ans=1;
+        while(pw!=0){
+            if(pw%2 !=0) ans*=base;
+            base*=base, pw/=2;
+        }
+        return ans;
+    }
+    friend Bigint pow(Bigint a, Bigint b,Bigint mod) {
+        if (b==0) return Bigint(1);
+        Bigint tmp=pow(a,b/2,mod);
+        if ((b%2)==0) return (tmp*tmp)%mod;
+        else return (((tmp*tmp)%mod)*a)%mod;
+    }
+    friend Bigint sqrt(Bigint x) {
+        Bigint ans=x,tmp=(x+1)/2;
+        while (tmp<ans) ans=tmp, tmp=(tmp+x/tmp)/2;
+        return ans;
+    }
+    friend Bigint gcd(Bigint a,Bigint b){
+        return a%b==0 ? b : gcd(b, a%b);
+    }
+    friend Bigint lcm(Bigint a,Bigint b){
+        return a/gcd(a,b);
+    }
+};
+
+ll intPow(int b, int x){ ll cur = 1; while(x--) cur *= b; return cur;}
+ll intLog10(int x){int res = 0; while(x) res++, x/=10; return res;}
+
+void solve(){
+
+	Bigint ans(0);
+
+	string s;	
+	while(!cin.eof()){
+		getline(cin, s);
+		if(s.back() < '0' or s.back() > '9') s.pop_back();
+		vector<ll> a = {0};
+
+		for(int i = 0; i < s.size(); i++){
+			if(s[i] == ':') continue;
+			if(s[i] == ' ') a.push_back(0);
+			else a.back() = a.back() * 10ll + (s[i] - '0');
+		}
+
+		cout << a[0] << " => ";
+		for(int i = 0; i < a.size(); i++) cout << a[i] << " ";
+		cout << endl;
+
+		int mxMax = intPow(3,((int)a.size()-2));
+
+		for(int m = 0; m < mxMax; m++){
+			ll cur = a[1];
+			// cout << ">> " << a[1] << " ";
+			int i;
+			for(i = 2; i < a.size(); i++){
+				int trit = (m/(intPow(3,i-2))) % 3;
+
+				if(trit == 0){
+					// cout << "+ ";
+					if(cur > LLMAX - a[i]) break;
+					cur += a[i];
+				}
+				else if(trit == 1){
+					// cout << "* ";
+					if(cur > LLMAX / a[i]) break;
+					cur *= a[i];
+				}
+				else{
+					// cout << "|| ";
+					if(intLog10(cur) + intLog10(a[i]) > 18) break;
+					cur *= intPow(10,intLog10(a[i]));
+					cur += a[i];
+				}
+				// cout << a[i] << " ";
+			}
+			// cout << "= " << cur << endl;
+			if(cur == a[0] and i >= a.size()){
+				ans += cur;
+				break;
+			}
+		}
+	}
+
+	cout << ans << endl;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t = 1;
+	while(t--){
+		solve();
+	}
+    return 0;
+}
